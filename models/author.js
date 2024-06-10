00e2f2efad3354ff190628535be6f747a4830dd9
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-10 17:42:07 +0700

    Add Author, Book, Bookinstance, and Genre models

---
+++ models/author.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100},
+  family_name: {type: String, required: true, maxLength: 100},
+  date_of_birth: {type: Date},
+  date_of_death: {type: Date},
+});
+
+authorSchema.virtual('name').get(function() {
+  let fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name};`
+  }
+
+  return fullname;
+});
+
+authorSchema.virtual('url').get(function() {
+  return  `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model('Author', authorSchema);
