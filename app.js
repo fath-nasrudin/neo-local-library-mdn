86c9f7347d06969c47e409b259b1da70d4363118
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-12 14:21:38 +0700

    Add compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +26,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
